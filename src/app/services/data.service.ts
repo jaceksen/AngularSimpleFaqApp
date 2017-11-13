535f67245da7816f50b70011fe8204ddc735f634
Jacek Seń <jaceksen@gmail.com>
2017-11-13 19:57:32 +0100

    dodany local storage

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -7,6 +7,7 @@ export class DataService {
   
 
   constructor() { 
+    /*
     this.questions = [
       {
         text:'what is your name?',
@@ -24,20 +25,44 @@ export class DataService {
         hide: true
       }
     ];
+    */
   }
 
   getQuestions(){
+    if(localStorage.getItem('questions') === null){
+      this.questions = [];
+    } else {
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
     return this.questions;
   }
 
   addQuestion(question:Question){
     this.questions.unshift(question);
+
+    //init local variable
+    let questions;
+
+
+    if(localStorage.getItem('questions') === null){
+      questions = [];
+      questions.unshift(question);
+      //save local array to local storage
+      localStorage.setItem('questions',JSON.stringify(questions));
+    } else {
+      questions = JSON.parse(localStorage.getItem('questions'));
+      //add new question
+      questions.unshift(question);
+      //save to local storaga
+      localStorage.setItem('questions',JSON.stringify(questions));
+    }
   }
 
   removeQuestion(question:Question){
     for(let i = 0; i < this.questions.length; i++){
       if(question == this.questions[i]){
         this.questions.splice(i,1);
+        localStorage.setItem('questions',JSON.stringify(this.questions));
       }
     }
   }
